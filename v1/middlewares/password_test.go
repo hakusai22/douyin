21c99ff668e92a76257dfd759f30b972359a15cd
yinpeng <y960303802@163.com>
2024-01-14 21:05:45 +0800

    [add] tiktok-v1 版本

--- v1/middlewares/password_test.go
+++ v1/middlewares/password_test.go
@@ -2,7 +2,7 @@ package middlewares
 
 import "testing"
 
-//40bd001563085fc35165329ea1ff5c5ecbdbbeef
+// 40bd001563085fc35165329ea1ff5c5ecbdbbeef
 func TestSHA1(t *testing.T) {
 	print(SHA1("123"))
 }
